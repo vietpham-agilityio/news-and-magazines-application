350683d15ba1c79c2f2e02140339c07213f374b9
Viet Pham <viet.pham@asnet.com.vn>
2024-05-20 10:38:12 +0700

    #22 Init next project

---
+++ practice-one/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "News & Magazine Application",
+  description:
+    "Stay updated with the latest news, in-depth articles, and engaging features from around the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
