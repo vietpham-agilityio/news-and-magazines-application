2bd775fdd737b91c3bd760cd9e122e430361d557
Viet Pham <viet.pham@asnet.com.vn>
2024-05-21 14:51:00 +0700

    #33 Split container of root Layout

--- practice-one/src/app/layout.tsx
+++ practice-one/src/app/layout.tsx
@@ -17,7 +17,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        {/* header */}
+      <header className="header container flex flex-col items-center justify-center bg-white-100 h-auto mx-auto mt-11 sm:mt-4 lg:mt-11 lg:flex-row">
+        <div className="h-12 w-full flex justify-between">
+          {/* brand and navigate */}
+        <div className="bg-white-5 w-12 h-12 sm:w-[664px]"></div>
+          {/*search and user*/}
+        <div className="bg-white-75 h-12 w-[300px] flex sm:hidden lg:flex lg:w-[656px]"></div>
+        </div>
+         {/*search on small screen*/}
+         <div className="bg-primary-100 container h-14 hidden sm:flex lg:hidden"></div>
+      </header>
+
+        {children}
+
+      {/* footer */}
+      <footer className="footer w-full hidden items-center justify-center bg-primary-50 h-[483px] mb-12.5 sm:flex lg:mb-11">
+        <p>footer</p>
+      </footer>
+
+      {/* footer mobile */}
+      <footer className="footer w-full flex items-center justify-center bg-primary-75 h-[70px] fixed bottom-0 sm:hidden">
+        <p>footer</p>
+      </footer>
+      </body>
     </html>
   );
 }
