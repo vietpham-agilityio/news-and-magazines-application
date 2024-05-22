6fe025cc0d76ab925a7b5dd8ba4fa90d9bab200a
Viet Pham <viet.pham@asnet.com.vn>
2024-05-22 10:28:59 +0700

    #29 Config font weight on rootLayout and modified variable on tailwind.config.ts

--- practice-one/src/app/layout.tsx
+++ practice-one/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto({ subsets: ["latin"], weight: "500" });
+const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400", "500", "700"] });
 
 export const metadata: Metadata = {
   title: "News & Magazine Application",
