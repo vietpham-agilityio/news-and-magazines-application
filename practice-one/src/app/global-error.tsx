a9ed91436e42261ab6b400e1a5ebb0d204c56d37
Viet Pham <viet.pham@asnet.com.vn>
2024-05-24 05:22:21 +0700

    #37 Define page GlobalError and Error page

---
+++ practice-one/src/app/global-error.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center mt-[177px] sm:mt-14 lg:mt-52 mb-7.5">
+      <h1 className="text-secondary-100 font-medium text-[162px] sm:text-[192px]">
+        Error
+      </h1>
+      <p className="text-sm text-dark-100 text-center mt-2">
+        Oops! Something went wrong we&apos;re sorry, but it looks like we&apos;ve hit a
+        snag.
+      </p>
+      <p className="text-sm text-dark-100 text-center mb-[85px] lg:mb-44">
+        The page you were trying to reach is currently unavailable.
+      </p>
+      <button className="mb-[291px] sm:mb-16" onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
