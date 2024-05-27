e9073051d09b5eda0bd78ca3af57f40653daed8c
Viet Pham <viet.pham@asnet.com.vn>
2024-05-28 04:45:06 +0700

    #50 Define Footer and FooterBar layout component

---
+++ practice-one/src/ui/layouts/FooterBar/index.tsx
@@ -0,0 +1,28 @@
+// constants
+import { flexCenter } from "@/constants";
+
+// components
+import { Button } from "@/ui/components/common";
+
+// icons
+import { Home, SaveTag, User, ChevronsUp } from "@/ui/components/Icons";
+
+export const FooterBar = () => (
+  <footer className="footer w-full flex items-center justify-between bg-white-100 h-[70px] fixed bottom-0 px-7.5 py-4 rounded-t-2xl shadow-inner sm:hidden">
+    <Button
+      label="Home"
+      textColor="text-white-100"
+      bgColor="bg-primary-90"
+      leftIcon={<Home />}
+    />
+    <div className={`${flexCenter} w-10 h-10`}>
+      <SaveTag />
+    </div>
+    <div className={`${flexCenter} w-10 h-10`}>
+      <User />
+    </div>
+    <div className={`${flexCenter} w-10 h-10`}>
+      <ChevronsUp />
+    </div>
+  </footer>
+);
