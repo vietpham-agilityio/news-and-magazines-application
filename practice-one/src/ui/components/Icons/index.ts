9d09c2868ac8c5b9b40f4867e0499db8423b4ed6
Viet Pham <viet.pham@asnet.com.vn>
2024-05-27 09:30:27 +0700

    #53 Define listCategories and ArrowTop icon

--- practice-one/src/ui/components/Icons/index.ts
+++ practice-one/src/ui/components/Icons/index.ts
@@ -1,5 +1,6 @@
-export * from './SaveTag';
 export * from './Search';
+export * from './SaveTag';
+export * from './ArrowTop';
 export * from './ThreeDots';
 export * from './BurgerMenu';
 export * from './ArrowBottom';
