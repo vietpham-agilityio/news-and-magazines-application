da6296852e2ae23a76ff43959b46f1820015f86b
Viet Pham <viet.pham@asnet.com.vn>
2024-05-30 14:15:16 +0700

    #71 Implement image for schedule section

--- practice-one/src/app/page.tsx
+++ practice-one/src/app/page.tsx
@@ -1,4 +1,11 @@
-import { TitleSection, CardPost } from "@/ui/components";
+// assets
+import ScheduleSmallImage from "@/images/widget/sports-widget-small.png";
+import ScheduleMediumImage from "@/images/widget/sports-widget-medium.png";
+import ScheduleLargeImage from "@/images/widget/sports-widget-large.png";
+
+// components
+import Image from "next/image";
+import { TitleSection, CardPost, CardSingle } from "@/ui/components";
 import { HashtagGroup } from "@/ui/features";
 
 export default function Home() {
@@ -9,18 +16,24 @@ export default function Home() {
         <HashtagGroup />
       </section>
       {/* fantastic posts */}
-      <section className="fantastic-posts container hidden items-center gap-6 bg-white-100 h-[452px] mt-9 mb-12.5 sm:flex lg:my-15">
+      <section className="container hidden items-center gap-6 bg-white-100 h-[452px] mt-9 mb-12.5 sm:flex lg:my-15">
         {/* first card */}
         <div className="hidden xl:block h-full">
-          <div className="h-full bg-blue-400 flex items-center justify-center w-[300px] xl:w-[360px] ">
-            <p>Fanstatic post</p>
-          </div>
+          <CardSingle
+            id="1"
+            title="Go Home After Long Time"
+            content="Download torrents from verified or trusted uploaders. If you're a BitTorrent user looking for safety tips, use this method. Both of the big-name BitTorrent indexers (The Pirate Bay and KickAssTorrents) use symbols to highlight torrents uploaded by verified users. "
+            imageSrc="https://images.unsplash.com/photo-1716223996696-ad6252a7fe7c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D"
+          />
         </div>
         {/* second card */}
         <div className="hidden xl:block h-full">
-          <div className="h-full bg-amber-700 flex items-center justify-center w-[300px] xl:w-[360px] ">
-            <p>Fanstatic post</p>
-          </div>
+          <CardSingle
+            id="2"
+            title="Street Way To The Conner"
+            content="Download torrents from verified or trusted uploaders. If you're a BitTorrent user looking for safety tips, use this method. Both of the big-name BitTorrent indexers (The Pirate Bay and KickAssTorrents) use symbols to highlight torrents uploaded by verified users. "
+            imageSrc="https://images.unsplash.com/photo-1716223996699-9738f0f12805?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D"
+          />
         </div>
         {/* slide card */}
         <div className="h-full bg-red-700 flex flex-1 items-center justify-center ">
@@ -30,7 +43,7 @@ export default function Home() {
       {/* popular posts */}
       <section className="popular-posts container flex flex-col justify-between bg-white-100 h-459 mt-7.5 sm:my-0">
         <TitleSection title="Popular Posts" />
-        <div className="card-group flex gap-6 flex-wrap">
+        <div className="card-group flex gap-6 flex-wrap overflow-hidden">
           <CardPost isVerticle />
           <CardPost isVerticle />
           <CardPost isVerticle />
@@ -38,13 +51,42 @@ export default function Home() {
         </div>
       </section>
       {/* scheduler */}
-      <section className="scheduler w-full flex items-center justify-center bg-primary-50 h-[464px] my-10 sm:mt-9 sm:mb-12.5 lg:my-17.5">
-        <p>scheduler</p>
+      <section className="w-full flex items-center justify-center my-10 sm:mt-9 sm:mb-12.5 lg:my-17.5">
+        <Image
+          src={ScheduleLargeImage}
+          alt="schedule"
+          className="hidden lg:block"
+          layout="responsive"
+          style={{
+            width: "full",
+            height: "auto",
+          }}
+        />
+        <Image
+          src={ScheduleMediumImage}
+          alt="schedule"
+          className="hidden sm:block lg:hidden"
+          layout="responsive"
+          style={{
+            width: "full",
+            height: "auto",
+          }}
+        />
+        <Image
+          src={ScheduleSmallImage}
+          alt="schedule"
+          className="sm:hidden"
+          layout="responsive"
+          style={{
+            width: "full",
+            height: "auto",
+          }}
+        />
       </section>
       {/* new posts */}
       <section className="new-posts container flex flex-col justify-between bg-white-100">
         <TitleSection title="New Posts" />
-        <div className="grid grid-cols-12 grid-rows-3 gap-6 xl:m-auto">
+        <div className="grid grid-cols-12 grid-rows-3 gap-6 xl:m-auto overflow-hidden">
           {/* card first */}
           <div className="col-span-12 2xl:col-span-6">
             <CardPost />
@@ -77,7 +119,7 @@ export default function Home() {
           <div>
             <p className="leading-10">Latest videos</p>
           </div>
-          <div className="card-group grid grid-cols-12 grid-rows-2 gap-6 mt-[30px]">
+          <div className="card-group grid grid-cols-12 grid-rows-2 gap-6 mt-[30px] overflow-hidden">
             {/* card first */}
             <div className="row-span-2 col-span-6 bg-dark-25 flex items-center justify-center">
               <p>Latest video</p>
@@ -102,7 +144,7 @@ export default function Home() {
         </div>
       </section>
       {/* trendy posts */}
-      <section className="trendy-posts container flex flex-col justify-between bg-white-100 h-459">
+      <section className="trendy-posts container flex flex-col justify-between bg-white-100 h-459 overflow-hidden">
         <TitleSection title="Trendy Posts" />
         <div className="card-group flex gap-6 flex-wrap">
           <CardPost isVerticle />
@@ -141,7 +183,7 @@ export default function Home() {
       {/* top posts */}
       <section className="top-posts container flex flex-col justify-between bg-white-100 h-459 mb-30 sm:mb-7.5 lg:mb-25">
         <TitleSection title="Top Posts" />
-        <div className="card-group flex gap-6 flex-wrap">
+        <div className="card-group flex gap-6 flex-wrap overflow-hidden">
           <CardPost isVerticle />
           <CardPost isVerticle />
           <CardPost isVerticle />
