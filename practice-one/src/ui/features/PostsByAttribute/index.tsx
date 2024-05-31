7666fecc5f2f08f8905b45ac9f863aabbfed28d9
Viet Pham <viet.pham@asnet.com.vn>
2024-05-31 17:22:23 +0700

    #75 Define PostByAttribute feature component

---
+++ practice-one/src/ui/features/PostsByAttribute/index.tsx
@@ -0,0 +1,43 @@
+import { memo } from "react";
+
+// service
+import { getPostDataByAttribute } from "@/services";
+
+// component
+import CardPost from "@/ui/components/Card/CardPost";
+
+// type
+import { PostVariant, PostResponse } from "@/types";
+import { CardType } from "@/types";
+
+export default memo(async function PostByAttribute({
+  attribute,
+  cardType,
+}: {
+  attribute: PostVariant;
+  cardType: CardType;
+}) {
+  const { data: postDataResponse } = await getPostDataByAttribute(attribute);
+  const typeCardPost = cardType === "isVertical";
+
+  return (
+    <div className="card-group flex gap-6 overflow-x-scroll">
+      {postDataResponse.map((post: PostResponse) => {
+        const { title, content, imageUrl, authorId, publicationDate } = post.attributes;
+        return (
+          <CardPost
+            isVertical={typeCardPost}
+            key={post.id}
+            id={post.id.toString()}
+            title={title}
+            alt={title}
+            content={content}
+            imageSrc={imageUrl}
+            authorId={authorId}
+            information={publicationDate}
+          />
+        );
+      })}
+    </div>
+  );
+});
