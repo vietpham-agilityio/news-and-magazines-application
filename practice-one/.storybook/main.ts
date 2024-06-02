2cb5a6fb24fe20584f81a3398e33f2622f7e38d9
Viet Pham <viet.pham@asnet.com.vn>
2024-06-02 10:43:53 +0700

    #83 Config TailWindCSS apply style for file storybook

--- practice-one/.storybook/main.ts
+++ practice-one/.storybook/main.ts
@@ -1,20 +1,34 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
-  stories: [
-    "../**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
   staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.resolve!.alias = {
+      ...config.resolve!.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+    return config;
+  },
 };
 export default config;
--- practice-one/.storybook/preview.tsx
+++ practice-one/.storybook/preview.tsx
@@ -1,12 +1,21 @@
 import type { Preview } from "@storybook/react";
+import 'tailwindcss/tailwind.css';
 
 const preview: Preview = {
   parameters: {
+    nextjs: { appDirectory: true },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#FFF" },
+        { name: "dark", value: "#333" },
+      ],
     },
   },
 };
