195456e0b9ece21bd6f197a4af13232d1abe702b
Viet Pham <viet.pham@asnet.com.vn>
2024-06-02 18:48:26 +0700

    #89 Implement Storybook and testing for Button

---
+++ practice-one/src/ui/components/common/Button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '.';
+import { ArrowRight } from '@/ui/components/Icons';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const ButtonWithIcon: Story = {
+  args: {
+    rightIcon: <ArrowRight />,
+    label: 'Next',
+    bgColor: 'bg-primary-100',
+    textColor: 'text-dark-100',
+    onClick: () => alert('Clicked'),
+  },
+};
+
+export const ButtonWithMarginBottom: Story = {
+  args: {
+    rightIcon: <ArrowRight />,
+    label: 'Next',
+    bgColor: 'bg-primary-100',
+    textColor: 'text-dark-100',
+    isMarginBottom: true,
+  },
+};
