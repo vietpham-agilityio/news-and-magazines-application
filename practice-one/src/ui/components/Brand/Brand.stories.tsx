144b3cd3713a445ecc87e18d10092e1e540e7f1b
Viet Pham <viet.pham@asnet.com.vn>
2024-06-02 10:46:35 +0700

    #83 Implement storybook for Brand component

---
+++ practice-one/src/ui/components/Brand/Brand.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Brand } from ".";
+
+const meta = {
+  title: "Components/Brand",
+  component: Brand,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Brand>;
+
+export default meta;
+type Story = StoryObj<typeof Brand>;
+
+export const BrandWithoutLink: Story = {
+  args: {
+   brandName: "News"
+  },
+};
+
+export const BrandWithLink: Story = {
+  args: {
+    brandName: "MEGA.news",
+    url: "https://storybook.js.org/"
+  }
+};
