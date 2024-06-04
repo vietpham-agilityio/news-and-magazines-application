59462ad51e4005446c94b1ddb8119500601af45b
Viet Pham <viet.pham@asnet.com.vn>
2024-06-04 10:51:56 +0700

    #110 Format code style on RightSidebar, PostDetail and TagGroup

--- practice-one/src/ui/features/RightSidebar/index.tsx
+++ practice-one/src/ui/features/RightSidebar/index.tsx
@@ -1,15 +1,15 @@
 // asset
-import AdvertisingOne from "@/images/advertising/advertising-one.png";
-import AdvertisingTwo from "@/images/advertising/advertising-two.png";
-import AvatarAuthor from "@/images/avatar/avatar-author.jpg";
+import AdvertisingOne from '@/images/advertising/advertising-one.png';
+import AdvertisingTwo from '@/images/advertising/advertising-two.png';
+import AvatarAuthor from '@/images/avatar/avatar-author.jpg';
 
 // componens
-import Image from "next/image";
-import { Avatar, Button, Information } from "@/ui/components";
-import { SidebarCard, TagGroup } from "@/ui/features";
+import Image from 'next/image';
+import { Avatar, Button, Information } from '@/ui/components';
+import { TagGroup, SidebarCard } from '@/ui/features';
 
 // icons
-import { ChatBox, PaperPlane, Plus, SaveTagSmall } from "@/ui/components/Icons";
+import { ChatBox, PaperPlane, Plus, SaveTagSmall } from '@/ui/components/Icons';
 
 export const RightSidebar = () => (
   <aside className="bg-white-100 lg:ml-[23px] lg:w-[360px]">
--- practice-one/src/ui/features/TagGroup/index.tsx
+++ practice-one/src/ui/features/TagGroup/index.tsx
@@ -1,11 +1,11 @@
 // types
-import { ITagItem, Size } from "@/types";
+import { ITagItem, Size } from '@/types';
 
 // mocks
-import { listTagItems } from "@/mocks";
+import { listTagItems } from '@/mocks';
 
 // components
-import { TitleSection, Typography } from "@/ui/components";
+import { TitleSection, Typography } from '@/ui/components';
 
 interface IProps {
   listTags?: ITagItem[];
@@ -15,11 +15,16 @@ export const TagGroup = ({ listTags = listTagItems }: IProps) => (
   <div className="h-full px-[15px] py-5 bg-white-90 rounded-xl">
     <TitleSection title="Tags" />
     <div className="flex flex-wrap gap-3.5 mt-3">
-      {listTags.map((tag) => {
+      {listTags.map(tag => {
         const { id, tagName } = tag;
 
         return (
-          <Typography key={id} tag="p" textSize={Size.XS} additionalClasses="text-dark-75 leading-4">
+          <Typography
+            key={id}
+            tag="p"
+            textSize={Size.XS}
+            additionalClasses="text-dark-75 leading-4"
+          >
             {tagName}
           </Typography>
         );
