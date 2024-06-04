4332ea37a06670550c22590f2a0f090b065b1a91
Viet Pham <viet.pham@asnet.com.vn>
2024-06-04 09:33:30 +0700

    #79 Define PostDetail component and implement feature show detail post

--- practice-one/src/app/articles/[id]/page.tsx
+++ practice-one/src/app/articles/[id]/page.tsx
@@ -1,59 +1,25 @@
 // componens
-import Image from 'next/image';
 import { RightSidebar } from '@/ui/features';
-import { TitleSection, Typography } from '@/ui/components';
+import { TitleSection } from '@/ui/components';
+import PostDetail from '@/ui/features/PostDetail';
 
 // services
-import { getPostDataById } from '@/services';
-import { CardType, FontWeight, PostVariant, Size } from '@/types';
+import { CardType, PostVariant } from '@/types';
 import PostsByAttribute from '@/ui/features/PostsByAttribute';
 
 export default async function Page({
   params: { id },
 }: {
   params: { id: number };
-})
- {
-  const postResponse = await getPostDataById(id);
-
-  const { title, content, imageUrl  } = postResponse.data.attributes;
+}) {
 
   return (
     <main className="container mx-auto">
       <p className="text-p font-normal my-11">Home</p>
-      <section className="flex flex-col lg:flex-row mb-12">
-        {/* article */}
-        <article className="flex-1 mb-17.5">
-          <div className="mb-7.5 lg:bg-white-90 lg:p-7 xl:mb-[130px] xl:h-[604px] rounded-xl">
-            <Typography
-              tag="h1"
-              additionalClasses="text-md font-medium sm:text-xxl sm:font-normal mb-7"
-            >
-              { title }
-            </Typography>
-            <Image
-              className="mx-auto rounded-xl w-[358px] h-[198px] sm:w-full sm:h-[336px] lg:h-[460px] xl:w-full xl:h-[538px] 2xl:h-[598px] 2xl:w-[1072px]"
-              src={imageUrl}
-              alt={title}
-              width={1072}
-              height={598}
-            />
-          </div>
-          <div className="w-[291px] sm:w-[516px] h-5 bg-slate-600 mx-auto mb-12">
-            <p>Tag</p>
-          </div>
-          <section>
-          {content?.split('\n').map((line, index) => (
-          <Typography key={index} tag='p' textSize={Size.MD} weight={FontWeight.Normal} additionalClasses='text-dark-100'>
-            { line }
-          </Typography>
-        ))}
-
-          </section>
-        </article>
-        {/* sidebar */}
+      <div className="flex flex-col lg:flex-row mb-12">
+        <PostDetail postId={id} />
         <RightSidebar />
-      </section>
+      </div>
 
       <section className="container flex flex-col justify-between bg-white-100 h-459 mt-17.5 mb-25">
         <TitleSection title="Popular Posts" />
---
+++ practice-one/src/ui/features/PostDetail/index.tsx
@@ -0,0 +1,59 @@
+import { memo } from 'react';
+
+// services
+import { getPostDataById } from '@/services';
+
+// componens
+import Image from 'next/image';
+import { Typography, Tag } from '@/ui/components';
+
+// type
+import { FontWeight, Size } from '@/types';
+import { ChatBox, File, Schedule } from '@/ui/components/Icons';
+
+export default memo(async function PostDetail({ postId }: { postId: number }) {
+  const postResponse = await getPostDataById(postId);
+
+  const { title, content, imageUrl } = postResponse.data.attributes;
+
+  return (
+    <section>
+      <article className="flex-1 mb-17.5">
+        <div className="mb-7.5 lg:bg-white-90 lg:p-7 xl:mb-[130px] xl:h-[604px] rounded-xl">
+          <Typography
+            tag="h1"
+            additionalClasses="text-md font-medium sm:text-xxl sm:font-normal mb-7"
+          >
+            {title}
+          </Typography>
+          <Image
+            className="mx-auto rounded-xl w-[358px] h-[198px] sm:w-full sm:h-[336px] lg:h-[460px] xl:w-full xl:h-[538px] 2xl:h-[598px] 2xl:w-[1072px]"
+            src={imageUrl}
+            alt={title}
+            width={1072}
+            height={598}
+          />
+        </div>
+        <div className="flex w-full justify-evenly mx-auto mb-12 xl:w-2/4">
+          <Tag icon={<Schedule />} content='July 14 , 2022' />
+          <Tag icon={<ChatBox />} content='Comments: 35' />
+          <Tag icon={<File />} content='Categories: Sport' />
+        </div>
+        <section>
+          {/* content */}
+          {content?.split('\n').map((line, index) => (
+            <Typography
+              key={index}
+              tag="p"
+              textSize={Size.MD}
+              weight={FontWeight.Normal}
+              additionalClasses="text-dark-100"
+            >
+              {line}
+            </Typography>
+          ))}
+        </section>
+      </article>
+    </section>
+  );
+});
