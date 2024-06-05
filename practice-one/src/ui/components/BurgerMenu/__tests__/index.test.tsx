88b50d684d59f36060ef0f0bf1a684d601573062
Viet Pham <viet.pham@asnet.com.vn>
2024-06-05 10:01:19 +0700

    #99 Implement Storybook and Testing for BurgerMenu component

---
+++ practice-one/src/ui/components/BurgerMenu/__tests__/index.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen, cleanup } from '@testing-library/react';
+
+// component
+import { BurgerMenu } from '@/ui/components';
+
+// mock data
+import { listNavItems } from '@/mocks';
+
+let renderBurgerMenu: any;
+
+describe('BurgerMenu component', () => {
+  beforeEach(() => {
+    renderBurgerMenu = render(<BurgerMenu listNav={listNavItems} />);
+  });
+
+  afterEach(() => {
+    renderBurgerMenu.unmount();
+    cleanup();
+  });
+
+  it('should render BurgerMenu macth snapshot', () => {
+    expect(renderBurgerMenu.asFragment()).toMatchSnapshot();
+  });
+
+  it('BurgerMenu should trigger event when clicked', () => {
+    const buttonElement = screen.getByRole('button');
+
+    fireEvent.click(buttonElement);
+
+    const { getByText } = renderBurgerMenu
+
+    const categoriesItem = getByText("Categories");
+    expect(categoriesItem).toBeInTheDocument();
+
+    fireEvent.click(categoriesItem);
+    const technologyItem = getByText("Technology");
+    expect(technologyItem).toBeInTheDocument();
+    fireEvent.click(technologyItem);
+  });
+});
