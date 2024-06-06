7089795e7ac70901e963f564efc1f7d91d2f3b3d
Viet Pham <viet.pham@asnet.com.vn>
2024-06-06 14:10:12 +0700

    #119 Remove ParperVideo and Video components unuse

--- practice-one/src/ui/components/index.ts
+++ practice-one/src/ui/components/index.ts
@@ -2,14 +2,12 @@
 export * from '@/ui/components/common';
 
 // shared
-export * from '@/ui/components/Video';
 export * from '@/ui/components/Brand';
 export * from '@/ui/components/NavItem';
 export * from '@/ui/components/Comment';
 export * from '@/ui/components/Carousel';
 export * from '@/ui/components/SearchBar';
 export * from '@/ui/components/PaperPost';
-export * from '@/ui/components/PaperVideo';
 export * from '@/ui/components/BurgerMenu';
 export * from '@/ui/components/Card/CardSingle';
 export * from '@/ui/components/Skeleton/CardPost';
