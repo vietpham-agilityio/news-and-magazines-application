1a9f5bf948278dbb755b5de2151332864ef4c33c
Viet Pham <viet.pham@asnet.com.vn>
2024-06-06 18:34:46 +0700

    #107 Define PostGrid feature component to render CardPost

--- practice-one/src/ui/features/index.ts
+++ practice-one/src/ui/features/index.ts
@@ -3,5 +3,6 @@ export * from '@/ui/features/SidebarCard';
 export * from '@/ui/features/RightSidebar';
 export * from '@/ui/features/HashtagGroup';
 
+export { default as PostsGrid } from '@/ui/features/PostsGrid';
 export { default as PostDetail } from '@/ui/features/PostDetail';
 export { default as PostsByAttribute } from '@/ui/features/PostsByAttribute';
