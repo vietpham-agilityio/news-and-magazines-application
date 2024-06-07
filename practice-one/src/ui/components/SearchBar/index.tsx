1fadc90914fcfa1db9fe10c8ee35b1315a7a18a8
Viet Pham <viet.pham@asnet.com.vn>
2024-06-07 13:55:13 +0700

    Remove react memo on CardPost, NavItem, SearchBar, PostDetail, PostsByAttribute and PostsGrid

--- practice-one/src/ui/components/SearchBar/index.tsx
+++ practice-one/src/ui/components/SearchBar/index.tsx
@@ -1,5 +1,3 @@
-import { memo } from "react";
-
 interface IProps {
   name: string;
   type?: string;
@@ -8,24 +6,22 @@ interface IProps {
   rightIcon?: React.JSX.Element;
 }
 
-export const SearchBar = memo(
-  ({ name, type, placeholder, leftIcon, rightIcon }: IProps) => (
-    <label htmlFor={name} className="relative flex-1">
-      <div className="absolute left-6 top-4">
-        { leftIcon }
-      </div>
-      <div className="absolute right-4 top-3">
-        { rightIcon }
-      </div>
-      <input
-        id={name}
-        type={type}
-        name={name}
-        className="w-full h-12 bg-white-90 placeholder-dark-100 rounded-xl outline-none px-14"
-        placeholder={placeholder}
-      />
-    </label>
-  )
+export const SearchBar = ({
+  name,
+  type,
+  placeholder,
+  leftIcon,
+  rightIcon,
+}: IProps) => (
+  <label htmlFor={name} className="relative flex-1">
+    <div className="absolute left-6 top-4">{leftIcon}</div>
+    <div className="absolute right-4 top-3">{rightIcon}</div>
+    <input
+      id={name}
+      type={type}
+      name={name}
+      className="w-full h-12 bg-white-90 placeholder-dark-100 rounded-xl outline-none px-14"
+      placeholder={placeholder}
+    />
+  </label>
 );
-
-SearchBar.displayName = "SearchBar";
