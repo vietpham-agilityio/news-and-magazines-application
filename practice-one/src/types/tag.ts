4a0c4de239f7f9ae19a413c862a67b3149ecaf97
Viet Pham <viet.pham@asnet.com.vn>
2024-06-08 19:04:20 +0700

    #143 Define listHashtagsItem, listPostItems constants and IHashtag type

--- practice-one/src/types/tag.ts
+++ practice-one/src/types/tag.ts
@@ -1,4 +1,12 @@
+import { StaticImageData } from "next/image";
+
 export interface ITagItem {
   id: string;
   tagName: string;
 };
+
+export interface IHashtag {
+  id: string;
+  name: string;
+  imageUrl: string | StaticImageData;
+}
