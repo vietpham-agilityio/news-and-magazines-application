0415be2819676843d64e41d7328c5714d976b48d
Viet Pham <viet.pham@asnet.com.vn>
2024-06-12 09:36:48 +0700

    #172 Add props count to reduce duplicate code on CardPostSkeleton

--- practice-one/src/ui/components/Skeleton/CardPost/index.tsx
+++ practice-one/src/ui/components/Skeleton/CardPost/index.tsx
@@ -1,32 +1,28 @@
-export const CardPostSkeleton = () => (
-  <div className="h-[385px] flex overflow-hidden">
-    <div className="animate-pulse mr-4">
-      <div className="bg-gray-300 h-46.5 w-90 mb-14 rounded-md" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-    </div>
-    <div className="animate-pulse mr-4">
-      <div className="bg-gray-300 h-46.5 w-90 mb-14 rounded-md" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-    </div>
-    <div className="animate-pulse mr-4">
-      <div className="bg-gray-300 h-46.5 w-90 mb-14 rounded-md" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-    </div>
-    <div className="animate-pulse mr-4">
-      <div className="bg-gray-300 h-46.5 w-90 mb-14 rounded-md" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
-      <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
-    </div>
+
+import React from 'react';
+
+interface IProps {
+  count: number;
+}
+
+export const CardPostSkeleton = ({ count }: IProps) => {
+  const skeletons = [];
+
+  for (let i = 0; i < count; i++) {
+    skeletons.push(
+      <div key={i} className="animate-pulse mr-4">
+        <div className="bg-gray-300 h-46.5 w-90 mb-14 rounded-md" />
+        <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
+        <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
+        <div className="h-6 bg-gray-300 rounded w-3/4 mb-2" />
+        <div className="h-6 bg-gray-300 rounded w-3/2 mb-2" />
+      </div>
+    );
+  }
+
+  return (
+  <div className="h-101 flex overflow-hidden">
+    { skeletons }
   </div>
-);
+  )
+};
