7fa6e755c48510bc1f4a7f034df3dbb04bdd0eac
Viet Pham <viet.pham@asnet.com.vn>
2024-06-19 17:10:35 +0700

    #201 Define CommentSkeleton

---
+++ practice-one/src/ui/components/Skeleton/Comment/index.tsx
@@ -0,0 +1,9 @@
+export const CommentSkeleton = () => (
+  <div className="">
+    <div className="animate-pulse">
+      <div className="bg-gray-300 h-15 w-30 rounded-md mb-4 sm:mb-3.5" />
+        <div className="h-8 bg-gray-300 rounded w-full mb-2" />
+        <div className="h-8 bg-gray-300 rounded w-full mb-2" />
+    </div>
+  </div>
+);
