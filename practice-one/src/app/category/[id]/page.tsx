92270cf6ca5791f831f35b4b13f7f7ccf5c4b880
Viet Pham <viet.pham@asnet.com.vn>
2024-06-19 12:24:02 +0700

    #192 Implement CardCategory inside Category page

--- practice-one/src/app/category/[id]/page.tsx
+++ practice-one/src/app/category/[id]/page.tsx
@@ -2,10 +2,10 @@
 import { flexItemCenter } from '@/constants';
 
 // mocks
-import { listTypePosts } from '@/mocks';
+import { listPaginationPage, listTypePosts } from '@/mocks';
 import { getCategoryById, getPostCategoryById } from '@/services';
-import { IBreadCrumbItem, CardType } from '@/types';
-import { BreadCrumbs, CardCategory, CardPost, TitleSection } from '@/ui/components';
+import { IBreadCrumbItem, CardType, Size, FontWeight } from '@/types';
+import { BreadCrumbs, CardCategory, CardPost, Pagination, TitleSection, Typography } from '@/ui/components';
 
 export default async function CategoryPage({
   params: { id },
@@ -33,44 +33,43 @@ export default async function CategoryPage({
     const isActive = index === 0;
 
     return (
-      <h6
+      <Typography
         key={type}
-        className={`text-xs pr-5 ${isActive ? 'text-primary-100' : 'text-dark-75'}`}
+        tag='h2'
+        textSize={Size.XS}
+        weight={FontWeight.Medium}
+        additionalClasses={`pr-5 ${isActive ? 'text-dark-100' : 'text-dark-75'}`}
       >
         {type}
-      </h6>
+      </Typography>
     );
   });
 
   return (
-    <main className="container mx-auto">
-      <div className="my-7.5 sm:my-12">
+    <main className="mx-2 sm:mx-6 lg:container lg:mx-auto">
+      <div className="ml-2 my-7.5 sm:my-11">
         <BreadCrumbs listBreadCrumb={listBreadCrumb} />
       </div>
       <div className={`${flexItemCenter} h-12 bg-white-90 rounded-xl p-4`}>
         {listTypeContents}
       </div>
       {/* new posts */}
-      <section className="new-posts container flex flex-col justify-between bg-white-100">
-        <div className="mt-7.5 mb-5 sm:mt-11 sm:mb-12">
-          <TitleSection title={`Category : ${categoryType}`}/>
+      <section className="new-posts lg:container flex flex-col justify-between bg-white-100">
+        <div className="mt-7.5 sm:mt-11 lg:mt-9">
+          <TitleSection isDisableButton title={`Category : ${categoryType}`} />
         </div>
         <div className="card-group grid grid-cols-12 grid-rows-3 gap-5 sm:gap-6">
             {postCategoriesDataResponse.map(post => {
               return (
-                <div className="col-span-12 lg:col-span-3" key={post.id}>
+                <div className="col-span-12 lg:col-span-6 xl:col-span-4 2xl:col-span-3" key={post.id}>
                 <CardCategory id={post.id}
                 />
-            </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
+        </div>
       </section>
-      <div
-        className={`h-10 bg-white-90 mt-10 mb-30 sm:mt-11 sm:mb-12 lg:mb-25 ${flexItemCenter}`}
-      >
-        <p>Navigation</p>
-      </div>
+      <Pagination listPagination={listPaginationPage} />
     </main>
   );
 }
--- practice-one/src/ui/components/TitleSection/index.tsx
+++ practice-one/src/ui/components/TitleSection/index.tsx
@@ -3,32 +3,36 @@ import { BoxIcon, Title } from '@/ui/components/common';
 
 interface IProps {
   title: string;
+  isDisableButton?: boolean;
   onClickArrowLeft?: () => void;
   onClickArrowRight?: () => void;
 }
 
 export const TitleSection = ({
   title,
+  isDisableButton = false,
   onClickArrowLeft,
   onClickArrowRight,
 }: IProps) => (
   <div className="mb-5 sm:mb-10 mx-6 flex justify-between lg:mx-0">
     <Title title={title} />
-    <div className="flex gap-5">
-      <BoxIcon
-        width="10"
-        height="10"
-        additionalClasses="hover:opacity-80"
-        icon={<ArrowLeft />}
-        onClick={onClickArrowLeft}
-      />
-      <BoxIcon
-        width="10"
-        height="10"
-        additionalClasses="hover:opacity-80"
-        icon={<ArrowRight color='#000' />}
-        onClick={onClickArrowRight}
-      />
-    </div>
+    {!isDisableButton && (
+      <div className="flex gap-5">
+        <BoxIcon
+          width="10"
+          height="10"
+          additionalClasses="hover:opacity-80"
+          icon={<ArrowLeft />}
+          onClick={onClickArrowLeft}
+        />
+        <BoxIcon
+          width="10"
+          height="10"
+          additionalClasses="hover:opacity-80"
+          icon={<ArrowRight color="#000" />}
+          onClick={onClickArrowRight}
+        />
+      </div>
+    )}
   </div>
 );
