e1bcfbc385a7326131b2f04df64f32fb9bb22a5b
Viet Pham <viet.pham@asnet.com.vn>
2024-06-21 16:26:52 +0700

    #204 Handle case empty comment on Detail page

--- practice-one/src/ui/features/CommentGroup/index.tsx
+++ practice-one/src/ui/features/CommentGroup/index.tsx
@@ -1,11 +1,12 @@
 // components
-import { Comment } from '@/ui/components';
+import { Comment, Typography } from '@/ui/components';
 
 // types
 import { getCommentByPostId } from '@/services';
 
 // utils
 import { formatDate } from '@/utils';
+import { FontWeight, Size } from '@/types';
 
 interface IProps {
   postId: string;
@@ -14,8 +15,22 @@ interface IProps {
 export default async function CommentGroup({ postId }: IProps) {
   const { data: listComments } = await getCommentByPostId(postId);
 
+  if (listComments.length === 0) {
+    return (
+      <div className="flex justify-center py-10">
+        <Typography
+          tag="h3"
+          weight={FontWeight.Light}
+          additionalClasses="text-sm text-black-75 leading-5"
+        >
+          This article has no comments
+        </Typography>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col gap-9 mt-7.5 mb-10 sm:mb-12 lg:mb-15">
+    <div className="flex flex-col gap-9 mb-10 sm:mb-12 lg:mb-15">
       {listComments.map(comment => {
         const { name, content, createdAt } = comment.attributes;
         return (
