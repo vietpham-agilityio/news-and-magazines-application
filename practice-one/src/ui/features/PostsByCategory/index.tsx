83c234840ff9e746f28d7a2dcf0f76bc7bfe0fb6
Viet Pham <viet.pham@asnet.com.vn>
2024-06-23 12:48:49 +0700

    #214 Handle lazy loading on Category page

--- practice-one/src/ui/features/PostsByCategory/index.tsx
+++ practice-one/src/ui/features/PostsByCategory/index.tsx
@@ -1,8 +1,5 @@
 // service
-import {
-  getCategoryById,
-  getPostCategoryById,
-} from '@/services';
+import { getCategoryById, getPostCategoryById } from '@/services';
 
 // type
 import { CardCategory, Pagination, TitleSection } from '@/ui/components';
@@ -14,18 +11,17 @@ export default async function PostByCategory({
   categoryId: number;
   pageIndex: number;
 }) {
-  const { data: categoryDataResponse } = await getCategoryById(categoryId);
-  const { data: postCategoriesDataResponse, meta } = await getPostCategoryById( categoryId, 12, pageIndex);
+  const { data: postCategoriesDataResponse, meta } = await getPostCategoryById(
+    categoryId,
+    12,
+    pageIndex
+  );
 
-  const { name: categoryType } = categoryDataResponse.attributes;
-  const { pageCount} = meta.pagination;
+  const { pageCount } = meta.pagination;
 
   return (
     <>
       <section className="new-posts lg:container flex flex-col justify-between bg-white-100">
-        <div className="mt-7.5 sm:mt-11 lg:mt-9">
-          <TitleSection isDisableButton title={`Category : ${categoryType}`} />
-        </div>
         <div className="card-group grid grid-cols-12 grid-rows-3 gap-5 sm:gap-6">
           {postCategoriesDataResponse.map(post => {
             return (
