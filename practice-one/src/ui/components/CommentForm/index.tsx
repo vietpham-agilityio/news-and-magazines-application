c4c52f1b7a258aebc7268d0213c4e060a8172b89
Viet Pham <viet.pham@asnet.com.vn>
2024-06-26 05:55:03 +0700

    #234 Add undefined condition on CommentForm

--- practice-one/src/ui/components/CommentForm/index.tsx
+++ practice-one/src/ui/components/CommentForm/index.tsx
@@ -27,66 +27,68 @@ export const CommentForm = ({ postId }: IProps) => {
       <div className="grid grid-cols-12 grid-rows-3 lg:gap-6">
         <div className="row-span-3 col-span-12 lg:col-span-6">
           <input name="postId" type="hidden" value={postId} />
-          <div className={`row-span-1 ${errorMessage.name ? 'mb-0' : 'mb-6'} `}>
+          <div
+            className={`row-span-1 ${errorMessage?.name ? 'mb-0' : 'mb-6'} `}
+          >
             <Input label="Name" name="name" type="text" />
-            {errorMessage.name && (
+            {errorMessage?.name && (
               <Typography
                 tag="h3"
                 textSize={Size.XSM}
                 weight={FontWeight.Light}
                 additionalClasses="ml-1 text-red-500 leading-6"
               >
-                {errorMessage.name}
+                {errorMessage?.name}
               </Typography>
             )}
           </div>
           <div
-            className={`row-span-1 ${errorMessage.email ? 'mb-0' : 'mb-6'} `}
+            className={`row-span-1 ${errorMessage?.email ? 'mb-0' : 'mb-6'} `}
           >
             <Input label="Email" name="email" type="email" />
-            {errorMessage.email && (
+            {errorMessage?.email && (
               <Typography
                 tag="h3"
                 textSize={Size.XSM}
                 weight={FontWeight.Light}
                 additionalClasses="ml-1 text-red-500 leading-6"
               >
-                {errorMessage.email}
+                {errorMessage?.email}
               </Typography>
             )}
           </div>
           <div
-            className={`row-span-1 ${errorMessage.website ? 'mb-0' : 'mb-6'} `}
+            className={`row-span-1 ${errorMessage?.website ? 'mb-0' : 'mb-6'} `}
           >
             <Input label="Website" name="website" type="text" />
-            {errorMessage.website && (
+            {errorMessage?.website && (
               <Typography
                 tag="h3"
                 textSize={Size.XSM}
                 weight={FontWeight.Light}
                 additionalClasses="ml-1 text-red-500 leading-6"
               >
-                {errorMessage.website}
+                {errorMessage?.website}
               </Typography>
             )}
           </div>
         </div>
         <div
-          className={`${errorMessage.content ? 'mb-0' : 'mb-6'} row-span-3 col-span-12 lg:col-span-6`}
+          className={`${errorMessage?.content ? 'mb-0' : 'mb-6'} row-span-3 col-span-12 lg:col-span-6`}
         >
           <TextArea
             name="content"
             label="Comment"
             placeholder="What do you think?"
           />
-          {errorMessage.content && (
+          {errorMessage?.content && (
             <Typography
               tag="h3"
               textSize={Size.XSM}
               weight={FontWeight.Light}
               additionalClasses="ml-1 text-red-500 leading-6"
             >
-              {errorMessage.content}
+              {errorMessage?.content}
             </Typography>
           )}
         </div>
